1786c5f7a64a4bdfba07636ddedc0eb093bcfa18
Wittawas Nakkasem <admin@vittees-MacBook-Pro.local>
2013-10-19 14:51:58 +0700

    Init commit

---
+++ src/Vittee/DebugbarDoctrine/DataCollector/Doctrine/PDODoctrineCollector.php
@@ -0,0 +1,30 @@
+<?php namespace Vittee\DebugbarDoctrine\DataCollector\Doctrine;
+
+use DebugBar\DataCollector\PDO\PDOCollector;
+
+class PDODoctrineCollector extends PDOCollector {
+    /**
+     * {@inheritDoc}
+     */
+    public function getName() {
+        return 'pdo-doctrine';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getWidgets()
+    {
+        return array(
+            "database" => array(
+                "widget" => "PhpDebugBar.Widgets.SQLQueriesWidget",
+                "map" => $this->getName(),
+                "default" => "[]"
+            ),
+            "database:badge" => array(
+                "map" => $this->getName().".nb_statements",
+                "default" => 0
+            )
+        );
+    }
+}
---
+++ src/Vittee/DebugbarDoctrine/DebugbarDoctrineServiceProvider.php
@@ -0,0 +1,71 @@
+<?php namespace Vittee\DebugbarDoctrine;
+
+use Illuminate\Support\ServiceProvider;
+use DebugBar\DataCollector\PDO\TraceablePDO;
+use Vittee\DebugbarDoctrine\DataCollector\Doctrine\PDODoctrineCollector;
+
+class DebugbarDoctrineServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+    	if ($this->app['config']->get('laravel-debugbar::config.enabled')) {
+
+    		$debugbar = $this->app['debugbar'];
+
+    		if ($debugbar && $this->app['config']->get('laravel-debugbar::config.enabled')) {
+				
+				if ($this->collects('db', true) and isset($this->app['db'])) { 
+					try {
+						$doctrine = $this->app['doctrine'];
+
+						if ($doctrine) {
+							$pdo = $doctrine->getConnection()->getWrappedConnection();
+							$collector = new PDODoctrineCollector(new TraceablePDO($pdo));
+							$debugbar->addCollector($collector);
+						}
+
+					} catch (\PDOException $e) {
+
+					}
+				}
+			}
+		}
+    }
+
+    public function collects($name, $default=false){
+        return $this->app['config']->get('laravel-debugbar::config.collectors.'.$name, $default);
+    }
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->package('vittee/debugbar-doctrine');		
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+
+
+}
